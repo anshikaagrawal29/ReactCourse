7db2571c619f2dc8b312dd3a62b29307033d8ab4
Anshika Agrawal <workwithanshika@gmail.com>
2022-07-20 16:26:33 +0530

    Adding ternary operator, learn how to add js inside html and pass functions from one component to another

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import About from "./component/About";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
+import React, {useState} from 'react';
 
 /*Added new Component and Learned PropTypes and Prop...*/
 function App() {
+  const[mode, setMode] = useState('light');
+  const[pageStyle, setStyle] = useState({
+    backgroundColor : 'grey'
+  })
+
+  const toggleModeClick = () =>
+  {
+      if(mode === 'dark')
+      {
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+      }
+      else
+      {
+        setMode('dark');
+        document.body.style.backgroundColor = '#adb5bd';
+      }
+  }
+
   return (
     <>
-    <Navbar title="Convert Text" aboutText = "About"/>
-    
-    <div className="container">
-    <TextForm/>
+    <Navbar title="Convert Text" aboutText = "About" mode={mode} toggleModeClick = {toggleModeClick}/>
+    <div className="container" mode={mode}>
+    <TextForm mode={mode}/>
       </div>
     </>
   );
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,14 +1,9 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
-export default function TextForm() {
+export default function TextForm(props) {
  const[text, setText] = useState("")
  const[btnText, setBtnText] = useState("Enable Dark Mode")
- const[enableDarkModeStyle, setEnableDarkModeStyle] = useState(
-    {
-        backgroundColor : 'white',
-        color : 'black'
-    }
- )
  
  const setUpValueOfMyText = () =>
  {
@@ -40,42 +35,18 @@ export default function TextForm() {
     setText("");
  }
 
- const enableDarkMode = () =>
- {
-    if(enableDarkModeStyle.backgroundColor === 'white')
-    {
-        setEnableDarkModeStyle({
-            backgroundColor : 'grey',
-            color : 'white'
-        })
-
-        setBtnText("Enable Light Mode")
-    }
-    else
-    {
-        setEnableDarkModeStyle({
-            backgroundColor : 'white',
-            color : 'black'
-        })
-
-        setBtnText("Enable Dark Mode")
-    }
-    
- }
-
   return (
    <>
-    <div className="container" style={enableDarkModeStyle}>
-    <h3 className="my-2 col-6 mx-10"> Enter Your Text Here:</h3>
+    <div className="container" style={{backgroundColor : props.mode === 'light'?'white':'#adb5bd', color: props.mode === 'light'?'black':'white'}}>
+    <h3 className="my-2 col-6 mx-10" > Enter Your Text Here:</h3>
          <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={onchangeOfMyText} id="myText" rows="8"></textarea>
+            <textarea className="form-control" style={{backgroundColor : props.mode === 'light'?'white':'#adb5bd', color: props.mode === 'light'?'black':'white'}} value={text} onChange={onchangeOfMyText} id="myText" rows="8"></textarea>
             <button type="button" onClick={setUpValueOfMyText} className="btn btn-primary btn-sm mx-2 my-2" >Convert to Upper Case</button>
             <button type="button" onClick={setLoValueOfMyText} className="btn btn-primary btn-sm mx-2 my-2">Convert to Lower  Case</button>
             <button type="button" onClick={clearMyText} className="btn btn-primary btn-sm mx-2 my-2">Clear Text</button>
-            <button type="button" onClick={enableDarkMode} className="btn btn-primary btn-sm mx-2 my-2">{btnText}</button>  
         </div>
     </div>
-     <div className="container">
+     <div className="container" style={{backgroundColor : props.mode === 'light'?'white':'#adb5bd', color: props.mode === 'light'?'black':'white'}}>
         <hr></hr>
         <h3> Your Summary:</h3>
         <p>Text words Count : {getLength()} and Text Length is: {text.length}</p>
