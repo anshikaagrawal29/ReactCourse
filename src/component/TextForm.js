3b088b6366e13b1dce4324e4bc26ff2b9aee0c3d
Anshika Agrawal <workwithanshika@gmail.com>
2022-07-26 14:26:23 +0530

    Add

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -30,7 +30,7 @@ export default function TextForm(props) {
     }
     else
     {
-      return text.split(" ").length;
+      return text.split(" ").filter((element) => { return element.length !== 0}).length;
     }
     
  }
@@ -45,9 +45,9 @@ export default function TextForm(props) {
     <h3 className="my-2 col-6 mx-10" > Enter Your Text Here:</h3>
          <div className="mb-3">
             <textarea className="form-control" style={{backgroundColor : props.mode === 'light'?'white':'#e7e9eb', color: props.mode === 'light'?'black':'black'}} value={text} onChange={onchangeOfMyText} id="myText" rows="8"></textarea>
-            <button type="button" onClick={setUpValueOfMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`} >Convert to Upper Case</button>
-            <button type="button" onClick={setLoValueOfMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`}>Convert to Lower  Case</button>
-            <button type="button" onClick={clearMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`}>Clear Text</button>
+            <button type="button" disabled={text.length === 0} onClick={setUpValueOfMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`} >Convert to Upper Case</button>
+            <button type="button" disabled={text.length === 0} onClick={setLoValueOfMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`}>Convert to Lower  Case</button>
+            <button type="button" disabled={text.length === 0} onClick={clearMyText} className={`btn btn-${props.btnColor} btn-sm mx-2 my-2`}>Clear Text</button>
         </div>
     </div>
      <div className="container" style={{backgroundColor : props.mode === 'light'?'white':'#e7e9eb', color: props.mode === 'light'?'black':'black'}}>
